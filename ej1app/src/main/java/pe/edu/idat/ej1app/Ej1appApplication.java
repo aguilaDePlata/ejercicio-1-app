d679e5b497e31410abf362de38241ed56d939907
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2023-06-18 09:42:30 -0500

    feature: inicializacion

---
+++ ej1app/src/main/java/pe/edu/idat/ej1app/Ej1appApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ej1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej1appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej1appApplication.class, args);
+	}
+
+}
---
+++ ej1app/src/test/java/pe/edu/idat/ej1app/Ej1appApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ej1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej1appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
